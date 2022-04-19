f80705776a040178986ce70b928f9951bf2d42d5
Mason Meyer <mason.k.meyer@gmail.com>
2022-04-19 07:44:34 -0600

    add logging to the http handler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/masonkmeyer/serve
+
+go 1.18
--- main.go
+++ main.go
@@ -9,11 +9,24 @@ import (
 func main() {
 	port := flag.String("p", "8080", "Port for the server")
 	dir := flag.String("d", ".", "Directory to server")
+	quiet := flag.Bool("q", false, "Set for quiet mode")
 
 	flag.Parse()
 
 	portDef := ":" + *port
 
 	log.Println("Listening at", "localhost"+portDef)
-	log.Fatal(http.ListenAndServe(portDef, http.FileServer(http.Dir(*dir))))
+	fileHandler := http.FileServer(http.Dir(*dir))
+
+	log.Fatal(http.ListenAndServe(portDef, logHandler(fileHandler, *quiet)))
+}
+
+func logHandler(h http.Handler, quiet bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !quiet {
+			log.Printf("%s %s\n", r.Method, r.URL.Path)
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
